330e2d28351f783353c7a04562ef3448ad6654f3
sanjib-sen <sksenonline@gmail.com>
2021-07-22 03:26:48 +0600

    cleaned and fixed left: local variable referenced before assignment

--- shellcalc/__main__.py
+++ shellcalc/__main__.py
@@ -3,43 +3,50 @@ import operator
 inp = "".join(sys.argv[1:])
 
 operators = ['^', '%', '/', '*', '+', '-']
-opera = {'/':operator.truediv,'+':operator.add,'-':operator.sub,'*':operator.mul, '^':operator.pow,'%':operator.mod}
+opera = {'/': operator.truediv, '+': operator.add, '-': operator.sub,
+         '*': operator.mul, '^': operator.pow, '%': operator.mod}
+
 
 def get_numbers(string, left, index):
     num1 = string[left+1:index]
-    if left == 0: num1 = string[left:index]
+    if left == 0:
+        num1 = string[left:index]
     right = len(string)
-    for i in range(index+1,len(string)):
+    for i in range(index+1, len(string)):
         char = string[i]
         if char in operators:
             right = i
             break
-    
+
     num2 = string[index+1:right]
     str1 = string[:left+1]
-    if left ==0 : str1=""
+    if left == 0:
+        str1 = ""
     str2 = string[right:]
     opr = string[index]
-    result = opera[opr](float(num1),float(num2))
+    result = opera[opr](float(num1), float(num2))
     right = len(str1+str(result))-2
     return str1+str(result)+str2+" "+str(right)
+
 
 def main(inp):
     inpcopy = inp
     for op in operators:
         ind = 0
-        while ind<len(inpcopy):
-            if inpcopy[ind] in operators and inpcopy[ind]!=op:
+        left = 0
+        while ind < len(inpcopy):
+            if inpcopy[ind] in operators and inpcopy[ind] != op:
                 left = ind
             if op == inpcopy[ind]:
                 index = ind
-                ret = get_numbers(inpcopy, left,index).split(" ")
+                ret = get_numbers(inpcopy, left, index).split(" ")
                 inpcopy = ret[0]
-                right =int(ret[1])
+                right = int(ret[1])
                 ind = right
                 continue
-            ind+=1
-        left =0
+            ind += 1
+        left = 0
     print(inpcopy)
+
 
 main(inp)
